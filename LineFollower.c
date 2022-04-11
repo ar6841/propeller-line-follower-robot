4ec69671bfcbaba31cc7775009bd5e93d8c32e24
Tzipora Ziegler <tziporaziegler@users.noreply.github.com>
2022-04-10 22:54:28 -0400

    Combine drive and checkIR cogs to followLine cog

--- LineFollower.c
+++ LineFollower.c
@@ -16,8 +16,7 @@
 #define TURN_DELAY      2000
 
 // Headers
-void drive();
-void checkIR();
+void followLine();
 void detectObject();
 void detectObstacle();
 void handleIntersectionDetected();
@@ -30,88 +29,35 @@ void stopWheels();
 void end();
 
 // Cogs
-volatile int *driveCog;
-volatile int *irCog;
+volatile int *followLineCog;
 volatile int *detectObjectCog;
 volatile int *detectObstacleCog;
-
-// Variables used to communicate between cogs
-volatile bool shouldTurnRight = false;
-volatile bool shouldTurnLeft = false;
-volatile bool shouldAdjustRight = false;
-volatile bool shouldAdjustLeft = false;
 
 int numIntersection = 0;
  
 int main()
 {
-  //printf("starting%d\n",123);
-  //simpleterm_close();
-  irCog = cog_run(checkIR, 128);
-  driveCog = cog_run(drive, 128);
-}
-
-void drive() {
-  while(1) {
-    
-    //simpleterm_open();
-    //printf("Left IR: %d\n", 123);
-    //printf("Right IR: %d\n", 123);
-    //simpleterm_close();    
-    
-    if(shouldTurnRight) {
-      turnRight();
-      shouldTurnRight = false;
-    } else if (shouldTurnLeft) {
-      turnLeft();
-      shouldTurnLeft = false;
-    } else if (shouldAdjustRight) {
-       adjustRight();
-       shouldAdjustRight= false;
-    } else if (shouldAdjustLeft) {
-      adjustLeft();
-      shouldAdjustLeft = false;
-    } else {
-      driveForward();
-    }
-  }
-}
-
-void checkIR() {
-  // Use to ensure not handling the same intersection multiple times
-  // TODO: confirm that this works
-  bool intersectionDetected = false;
-  
+  // print("Starting");
+  // simpleterm_close();
  
-  while(1) {
-    if(shouldAdjustRight || shouldAdjustLeft || shouldTurnRight || shouldTurnLeft) {
-      return;
-    } 
-    
+  followLineCog = cog_run(followLine, 128);
+}
+
+void followLine() {
+  driveForward();
+ 
+  while(1) {   
     int leftIR = input(LEFT_IR_PIN); 
     int rightIR = input(RIGHT_IR_PIN);
-    
-    /*pause(200);
-    simpleterm_open();
-    pause(200);
-    printf("Left IR: %d\n", leftIR);
-    printf("Right IR: %d\n", rightIR);*/
-    //simpleterm_close(); 
          
     if(leftIR == 1 && rightIR == 0) {
-      shouldAdjustLeft = true;
-      intersectionDetected = false;
+      adjustLeft();
     } else if(leftIR == 0 && rightIR == 1) {
-      shouldAdjustRight = true;
-      intersectionDetected = false;
+      adjustRight();
     } else if(leftIR == 0 && rightIR == 0) {
-      intersectionDetected = true;
       handleIntersectionDetected();
-    } else {
-      intersectionDetected = false;
     }
-  }
- 
+  } 
 }  
 
 void detectObject() {
@@ -149,25 +95,25 @@ void handleIntersectionDetected() {
 
   switch(numIntersection) {
     case 2: // i1
-      shouldTurnLeft = true;
+      turnLeft();
       break;
     case 6: // B4
     case 8: // A4
     case 11: // A1
     case 13: // B1
-      shouldTurnRight = true;
+      turnRight();
       break;
     case 3: // B1
-      shouldTurnRight = true;
-      //detectObjectCog = cog_run(detectObject, 128);
-      //detectObstacleCog = cog_run(detectObstacle, 128);
+      turnRight();
+      // detectObjectCog = cog_run(detectObject, 128);
+      // detectObstacleCog = cog_run(detectObstacle, 128);
       break;
-     case 17: // B5
+    case 17: // B5
        //end();
        break;
-     default:
-      //driveForward();
-      //pause(200);
+    default:
+      driveForward();
+      pause(450); // Clear interesection
       break;
   }
 }
@@ -214,8 +160,7 @@ void stopWheels() {
 void end() {
   stopWheels();
  
-  cog_end(driveCog);
-  cog_end(irCog);
+  cog_end(followLine);
 
   // TODO: celebrate
 }

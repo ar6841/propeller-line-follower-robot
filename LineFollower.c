76a44c67c8d0f6c42f20f3519f6281b1a7cd8271
Arjun Raja <ar6841@nyu.edu>
2022-04-13 19:19:25 -0400

    Line following loop finished

--- LineFollower.c
+++ LineFollower.c
@@ -2,18 +2,27 @@
 #include "servo.h"     
 #include <stdbool.h>
 #include <stdio.h>
+#include "ping.h"
 
-#define RIGHT_WHEEL_PIN 12
-#define LEFT_WHEEL_PIN  13
-#define LEFT_IR_PIN     16
-#define RIGHT_IR_PIN    17
+#define RIGHT_WHEEL_PIN         12
+#define LEFT_WHEEL_PIN          13
+#define LEFT_IR_PIN             16
+#define RIGHT_IR_PIN            17
 
-#define FORWARD_SPEED   15
-#define STOP_SPEED      0
-#define ADJUST_SPEED    25
-#define ADJUST_DELAY    100
-#define TURN_SPEED      30
-#define TURN_DELAY      2000
+#define RIGHT_ULTRASONIC_TRIG   11
+#define RIGHT_ULTRASONIC_ECHO   8
+#define LEFT_ULTRASONIC_PIN     0
+#define OBJECT_DETECTION_LED    3
+#define OBSTACLE_DETECTION_LED  6
+
+#define MAX_OBJECT_DISTANCE     20
+
+#define FORWARD_SPEED           35
+#define STOP_SPEED              0
+#define ADJUST_SPEED            20
+#define ADJUST_DELAY            10
+#define TURN_SPEED              50
+#define TURN_DELAY              1500
 
 // Headers
 void followLine();
@@ -36,45 +45,72 @@ volatile int *detectObstacleCog;
 int numIntersection = 0;
  
 int main()
-{
+{ 
   // print("Starting");
   // simpleterm_close();
  
   followLineCog = cog_run(followLine, 128);
+  //while(1){
+    //  driveForward();
+    //stopWheels();
+    //followLine();
+   
+    //    int cmDist = ping_cm(LEFT_ULTRASONIC_PIN);
+      //  print("D %d\n",cmDist);
+       // pause(200);
+
+ // }    
 }
 
 void followLine() {
-  driveForward();
- 
-  while(1) {   
+  //driveForward();   
+  while(1){
     int leftIR = input(LEFT_IR_PIN); 
     int rightIR = input(RIGHT_IR_PIN);
+   //printf("\n%d",leftIR);
+   //printf("\n%d",rightIR);
+   //pause(1000);
          
     if(leftIR == 1 && rightIR == 0) {
       adjustLeft();
     } else if(leftIR == 0 && rightIR == 1) {
       adjustRight();
-    } else if(leftIR == 0 && rightIR == 0) {
+    } else if(leftIR == 1 && rightIR == 1) {
       handleIntersectionDetected();
     }
-  } 
+    else{
+      driveForward();
+    }     
+  }         
 }  
 
 void detectObject() {
   int numObjectsDetected = 0;
+  bool shouldDetectObject = true;
 
   while(1) {
-    // TODO: ping left ultrasonic distance sensor
-    bool objectDetected = false;
+    int cmDist = ping_cm(LEFT_ULTRASONIC_PIN);
+    bool objectDetected = cmDist != 0 && cmDist < MAX_OBJECT_DISTANCE;
+    
     if(objectDetected) {
+      if(shouldDetectObject) {
+       shouldDetectObject = false;
        numObjectsDetected++;
-        // TODO: indicated object detected
        
-        if(numObjectsDetected == 2) {
-          cog_end(detectObjectCog);
-          end(); // TODO: remove if we still need to find the obstacle
-        }
+       high(OBJECT_DETECTION_LED);
+       pause(500);
+       low(OBJECT_DETECTION_LED);
+              
+       if(numObjectsDetected == 2) {
+         end();
+         cog_end(detectObjectCog);
+       }
+     }       
+    } else {
+      shouldDetectObject = true;
     }
+    
+    pause(200);      
   }
 }
 
@@ -94,22 +130,27 @@ void handleIntersectionDetected() {
   // TODO: Show intersection detected on LCD
 
   switch(numIntersection) {
-    case 2: // i1
+    case 3: // i1
       turnLeft();
       break;
-    case 6: // B4
-    case 8: // A4
-    case 11: // A1
-    case 13: // B1
+    case 4: // B1
       turnRight();
-      break;
-    case 3: // B1
-      turnRight();
-      // detectObjectCog = cog_run(detectObject, 128);
+      detectObjectCog = cog_run(detectObject, 128);
       // detectObstacleCog = cog_run(detectObstacle, 128);
       break;
-    case 17: // B5
-       //end();
+    case 7:
+      //if loop completed go forward
+      turnRight();
+      break; // B4
+    case 9:
+      turnRight();
+      break; // A4
+    case 12: // A1
+    case 14: // B1
+      turnRight();
+      break;
+    case 18: // B5
+       end();
        break;
     default:
       driveForward();
@@ -120,28 +161,28 @@ void handleIntersectionDetected() {
 
 void driveForward() {
   servo_speed(RIGHT_WHEEL_PIN, FORWARD_SPEED * -1);
-  servo_speed(LEFT_WHEEL_PIN, FORWARD_SPEED);   
+  servo_speed(LEFT_WHEEL_PIN, FORWARD_SPEED-7);   
 }  
 
 void adjustRight() {
   servo_speed(RIGHT_WHEEL_PIN, STOP_SPEED);
   servo_speed(LEFT_WHEEL_PIN, ADJUST_SPEED); 
   pause(ADJUST_DELAY);  
-  driveForward();
+  //driveForward();
 }  
 
 void adjustLeft() {
   servo_speed(RIGHT_WHEEL_PIN, ADJUST_SPEED * -1);
   servo_speed(LEFT_WHEEL_PIN, STOP_SPEED);   
   pause(ADJUST_DELAY);
-  driveForward();
+  //driveForward();
 }  
 
 void turnRight(){
   servo_speed(RIGHT_WHEEL_PIN, STOP_SPEED);
   servo_speed(LEFT_WHEEL_PIN, TURN_SPEED); 
-  pause(TURN_DELAY);  
-  driveForward();
+  pause(TURN_DELAY+250);  
+  //driveForward();
 } 
 
  
@@ -149,7 +190,7 @@ void turnLeft(){
   servo_speed(RIGHT_WHEEL_PIN, TURN_SPEED * -1);
   servo_speed(LEFT_WHEEL_PIN, STOP_SPEED);   
   pause(TURN_DELAY);
-  driveForward();
+  //driveForward();
 }
  
 void stopWheels() {
